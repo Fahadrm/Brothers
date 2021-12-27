3297ef4999c6f838f661e71ac1e1534b71c383c8
Hasna <73275743+hasna-has@users.noreply.github.com>
2021-12-27 11:44:32 +0530

    Add files via upload

--- brothers_invoice_print/__manifest__.py
+++ brothers_invoice_print/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','account','free_qty','report_qweb_pdf_watermark','customer_product_qrcode','product_mrp','stock_invoice_lot'],
+    'depends': ['base','account','free_qty','report_qweb_pdf_watermark','customer_product_qrcode','product_mrp','stock_invoice_lot','product_expiry'],
 
     # always loaded
     'data': [
