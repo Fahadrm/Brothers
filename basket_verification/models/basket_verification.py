6bb9643736afadd5565c01b2cfca6f45f6e1a0da
Hasna <73275743+hasna-has@users.noreply.github.com>
2021-12-29 08:24:44 +0530

    Add files via upload

--- basket_verification/models/basket_verification.py
+++ basket_verification/models/basket_verification.py
@@ -7,17 +7,35 @@ class BasketVerification(models.TransientModel):
     _description = 'Basket Verification'
 
     name = fields.Many2one("stock.basket", string="Basket")
+    picking_id = fields.Many2one('stock.picking',string='Picking')
+    partner_id = fields.Many2one('res.partner',related="picking_id.partner_id", string='Customer')
 
+    @api.onchange('name')
+    def onchange_name(self):
+        for res in self:
+            basket_id = self.env['stock.basket'].search([('name','=',res.name.id),('status','=','occupy')])
+            res.picking_id = basket_id.picking_id.id
 
     def set_basket_free(self):
-        print(self.name)
         for baskets in self:
-            baskets.name.write({'status': 'vacant'})
-            baskets.name.status='vacant'
-        # return
-
-    # def print_demo(self):
-    #     print("hiiii")
-
-
-
+            basket_id = self.env['stock.basket'].search([('name','=',baskets.name.id),('status','=','occupy')])
+            picking_id = basket_id.picking_id
+            if basket_id:
+                remove_basket = picking_id.write({'basket_ids':[(3,basket_id.id)]})
+                baskets.name.write({'status': 'vacant',
+                                    'picking_id':None})
+                baskets.name = None
+            else:
+                raise UserError(_('The selected basket han not yet allocated.'))
+                baskets.name = None
+        return {
+    'name': 'Basket Verification',
+    'view_mode': 'form',
+    'view_id': False,
+    'res_model': self._name,
+    'domain': [],
+    'context': dict(self._context, active_ids=self.ids),
+    'type': 'ir.actions.act_window',
+    'target': 'new',
+    'res_id': self.id,
+}
